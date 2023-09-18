7a81904bb07613a9ddb0e8c8fa98b85f91df09c6
DESKTOP-DU2RDFE\Admin <luudat214@gmail.com>
2023-09-18 15:29:11 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// eslint-disable-next-line
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      <img src={'../img/donatepls.jpg'} className="App-logo" alt="" />
+        <p>
+          Đô nết để ủng hộ nhóm nhé ❤️
+        </p>
+        <a
+          className="App-link"
+          href="./img/tusenavoicenhat.mp4"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Donate
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <video src="/public/img/public/img/9convert.com - Tú Sena nhưng anh ấy đòi tiền donate.mp4" controls></video>
+  </React.StrictMode>,
+);
+
+reportWebVitals();
