d2ed92096a8e90fd36d706cb3f13a4d3f5ef8fbd
BetoAnh <vtahvp@gmail.com>
2023-10-02 17:17:00 +0700

    Cap nhat footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,24 +2,31 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Footer = () => {
+    const handleScrollToTop = (event) => {
+        event.preventDefault();
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      };
     return (
         <div className=" bottom-0 left-0 w-full h-[600px] bg-black pt-[30px]">
             <div className="flex h-[500px] ml-[50px] mr-[50px] border-gray-600 border-b-2">
-                <div className="w-3/5 h-full flex justify-start items-start mt-[50px]">
+                <div className="w-3/5 flex justify-start items-start mt-[50px]">
                     <NavLink to="/Home" className="w-auto">
                         <div className="flex justify-start items-start">
                             <img src="./img/logoPage.png" alt="logo" className="w-[70%] h-[100%]" />
                         </div>
                     </NavLink>
-                    <span className="w-3/5 h-auto text-white text-left text-[20px] ml-[50px] mr-[180px] mt-[40px] whitespace-normal ">
+                    <span className="w-3/5 hidden md:flex h-auto text-white text-left text-[20px] ml-[50px] mr-[100px] mt-[40px] whitespace-normal ">
                         Mang lại trải nghiệm giải trí tuyệt vời trong từng thước phim
                     </span>
                 </div>
 
                 <div className="w-full flex">
                     {/*NỘI DUNG*/}
-                    <div className="w-1/3 h-full flex items-start flex-col  mr-[30px]">
-                        <span className="text-white text-[35px]  mt-[30px] mb-[30px]">Nội dung</span>
+                    <div className="w-1/3 h-full hidden md:flex items-start justify-start flex-col  mr-[30px]">
+                        <span className="text-white text-left text-[30px] mb-[30px]">Nội dung</span>
                         <div className="inline-block my-[15px] text-left">
                             <NavLink to="/Home" className="text-white text-[20px] mt-[30px]">
                                 <span>Trang chủ</span>
@@ -38,8 +45,8 @@ const Footer = () => {
                     </div>
 
                     {/*DỊCH VỤ*/}
-                    <div className="w-1/3 h-full flex items-start flex-col  mr-[30px]  ">
-                        <span className="text-white text-[35px]  mt-[30px] mb-[30px]">Dịch vụ</span>
+                    <div className="w-1/3 h-full hidden md:flex items-start justify-start flex-col  mr-[25px]  ">
+                        <span className="text-white text-left text-[30px] mb-[30px]">Dịch vụ</span>
                         <div className="inline-block my-[15px] text-left">
                             <NavLink to="/Home" className="text-white text-[20px] mt-[30px]">
                                 <span>Trang chủ</span>
@@ -58,8 +65,8 @@ const Footer = () => {
                     </div>
 
                     {/*HỖ TRỢ*/}
-                    <div className="w-1/3 h-full flex items-start flex-col mr-[60px]">
-                        <span className="text-white text-[35px] mt-[30px] mb-[30px]">Hỗ trợ</span>
+                    <div className="w-1/2 h-full hidden md:flex items-start justify-start flex-col mr-[30px]">
+                        <span className="text-white text-left text-[30px] mb-[30px]">Hỗ trợ</span>
                         <div className="inline-block my-[15px] text-left">
                             <NavLink to="/Home" className="text-white  text-[20px] mt-[30px]">
                                 <span>FAQs</span>
@@ -88,8 +95,8 @@ const Footer = () => {
                     </div>
 
                     {/*Công ty*/}
-                    <div className="w-[60%] h-full flex items-start flex-col ">
-                        <span className="text-white text-left text-[35px] mt-[30px] mb-[50px]">Công ty trách nhiệm hữu hạn một mình tao</span>
+                    <div className="w-[70%] h-full flex items-start justify-start flex-col overflow-hidden">
+                        <span className="text-white text-left text-[30px] mb-[30px]">Công ty trách nhiệm hữu hạn một mình tao</span>
                         <span className="text-white text-left text-[20px]">Địa chỉ: 123 Trần Duy Hưng, Hong Kong, California, Trung tâm kiểm soát động vật hoang dã, New York,dwajkdah ưqjdhqk</span>
                         <span className="text-white text-[20px] mt-[30px]">Giới hạn trách nhiệm</span>
                         <div className="inline-block my-[15px] mt-[30px]">
@@ -99,17 +106,34 @@ const Footer = () => {
                             </NavLink>
                         </div>
                     </div>
-
-
                 </div>
             </div>
-            <div className=" h-[50px] mr-[50px] ml-[50px] ">
+            <div className=" h-[70px] mr-[50px] ml-[50px] flex items-center justify-center">
                 <div className=" w-1/3">
                     <NavLink to="/Home">
                         <span className="text-white flex justify-start ml-[50px]">Xem anime</span>
                     </NavLink>
                 </div>
-
+                <div className="w-2/3 flex justify-end">
+                    <a href="https://www.facebook.com/"><div className="w-[40px] h-[40px] bg-neutral-500 hover:bg-neutral-600 mr-4 rounded flex items-center justify-center">
+                    <img src="./img/SocialNetworkLogos/facebook-f.svg" alt="facebook" className="w-2/3 h-2/3 text-cyan-400"/>
+                    </div></a>
+                    <a href="https://www.tiktok.com/"><div className="w-[40px] h-[40px] bg-neutral-500 hover:bg-neutral-600 mr-4 rounded flex items-center justify-center">
+                    <img src="./img/SocialNetworkLogos/tiktok.svg" alt="tiktok" className="w-2/3 h-2/3 text-cyan-400"/>
+                    </div></a>
+                    <a href="https://www.twitter.com/"><div className="w-[40px] h-[40px] bg-neutral-500 hover:bg-neutral-600 mr-4 rounded flex items-center justify-center">
+                    <img src="./img/SocialNetworkLogos/twitter.svg" alt="twitter" className="w-2/3 h-2/3 text-cyan-400"/>
+                    </div></a>
+                    <a href="https://www.instagram.com/"><div className="w-[40px] h-[40px] bg-neutral-500 hover:bg-neutral-600 mr-4 rounded flex items-center justify-center">
+                    <img src="./img/SocialNetworkLogos/instagram.svg" alt="instagram" className="w-2/3 h-2/3 text-cyan-400"/>
+                    </div></a>
+                    <a href="https://www.youtube.com/"><div className="w-[40px] h-[40px] bg-neutral-500 hover:bg-neutral-600 mr-4 rounded flex items-center justify-center">
+                    <img src="./img/SocialNetworkLogos/youtube.svg" alt="youtube" className="w-2/3 h-2/3 text-cyan-400"/>
+                    </div></a>
+                </div>
+                <a href="#"  onClick={handleScrollToTop} ><div className="w-[40px] h-[40px] bg-lime-500 hover:bg-lime-600 mr-4 rounded flex items-center justify-center">
+                <img src="./img/SocialNetworkLogos/arrow-up-solid.svg" alt="back-up" className="w-2/3 h-2/3 text-cyan-400"/>
+                </div></a>
             </div>
         </div>
     );
