d4f0f69318c730992ee1b74a20779f687bdf4d9e
MegaShork <qthanhbui13@gmail.com>
2023-10-02 10:30:32 +0700

    update Profile

--- src/Component/User/Profile/Profile.jsx
+++ src/Component/User/Profile/Profile.jsx
@@ -1,18 +1,30 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faUserCheck } from '@fortawesome/free-solid-svg-icons';
 import Header from '../../Header/Header';
+import ItemProfile from './itemProfile';
+import Footer from '../../Footer/Footer';
 
 const Profile = () => {
-
-    useEffect (() => {
+    const [avatar, setAvatar] = useState('./img/avatarUser.jpg');
+    const handleEditAvatar = () => {
+        document.getElementById("avatarInput").click();
+    }
+    const handelImageChange = (e) => {
+        const selectedImage = e.target.files[0];
+        if (selectedImage) {
+            const imageUrl = URL.createObjectURL(selectedImage);
+            setAvatar(imageUrl);
+        }
+    }
+    useEffect(() => {
         document.title = "Thông tin cá nhân";
-      },[]);
+    }, []);
 
     return (
         <div>
             <div>
-                <Header/>
+                <Header />
             </div>
             <div className="flex mt-[100px]">
                 <aside className="bg-white p-4 m-4 h-screen w-1/4 border-none">
@@ -24,77 +36,54 @@ const Profile = () => {
                         </li>
                     </ul>
                 </aside>
-
                 <main className="p-4 w-3/4">
                     <div className="bg-white p-6 border-none h-screen">
+                        <ItemProfile title={"Họ và Tên"} placeholder={"Thêm tên của bạn"} />
                         <div className="mb-4">
                             <div className="flex justify-between py-[16px]">
                                 <div className='flex flex-col items-start'>
-                                    <h3 className="text-xl font-semibold">Họ và tên</h3>
-                                    <input type="text" className="border-b border-gray-200 focus:outline-none py-[5px] px-[10px] w-[200%]" />
-                                </div>
-                                <div>
-                                    <button className="bg-red-500 text-white px-3 py-1 rounded-full">
-                                        Chỉnh sửa
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="mb-4">
-                            <div className="flex justify-between py-[16px]">
-                                <div className='flex flex-col items-start'>
-                                    <h3 className="text-xl font-semibold">Avatar</h3>
+                                    <h3 className="text-xl font-semibold">Ảnh đại diện</h3>
                                     <div className='flex justify-between'>
                                         <div>Nên là ảnh vuông, chấp nhận các tệp: JPG, PNG hoặc GIF.</div>
-                                        <img src="./img/avatarUser.jpg" alt="" className='w-[90px] h-[90px] ml-[90px]' />
+                                        <img src={avatar} alt="" className='w-[90px] h-[90px] ml-[90px]' />
                                     </div>
                                 </div>
                                 <div>
-                                    <button className="bg-red-500 text-white px-3 py-1 rounded-full">
+                                    <input 
+                                        type="file"
+                                        id='avatarInput'
+                                        accept='.jpg, .png, .gif'
+                                        style={{display: 'none'}}
+                                        onChange={handelImageChange}
+                                    />
+                                    <button 
+                                        onClick={handleEditAvatar}
+                                        className="bg-red-500 text-white px-3 py-1 rounded-full"
+                                    >
                                         Chỉnh sửa
                                     </button>
                                 </div>
                             </div>
                         </div>
+                        <ItemProfile title={"Email"} placeholder={"Thêm email của bạn"} />
+                        <ItemProfile title={"Số điện thoại"} placeholder={"Thêm số điện thoại của bạn"} />
                         <div className="mb-4">
                             <div className="flex justify-between py-[16px]">
                                 <div className='flex flex-col items-start'>
-                                    <h3 className="text-xl font-semibold">Email</h3>
-                                    <input type="text" className="border-b border-gray-200 focus:outline-none py-[5px] px-[10px] w-[200%]" />
-                                </div>
-                                <div>
-                                    <button className="bg-red-500 text-white px-3 py-1 rounded-full">
-                                        Chỉnh sửa
-                                    </button>
-                                </div>
-                            </div>
-                        </div><div className="mb-4">
-                            <div className="flex justify-between py-[16px]">
-                                <div className='flex flex-col items-start'>
-                                    <h3 className="text-xl font-semibold">Số điện thoại</h3>
-                                    <input type="text" className="border-b border-gray-200 focus:outline-none py-[5px] px-[10px] w-[200%]" />
-                                </div>
-                                <div>
-                                    <button className="bg-red-500 text-white px-3 py-1 rounded-full">
-                                        Chỉnh sửa
-                                    </button>
-                                </div>
-                            </div>
-                        </div><div className="mb-4">
-                            <div className="flex justify-between py-[16px]">
-                                <div className='flex flex-col items-start'>
-                                    <h3 className="text-xl font-semibold">Username</h3>
-                                    <input type="text" className="border-b border-gray-200 focus:outline-none py-[5px] px-[10px] w-[200%]" />
-                                </div>
-                                <div>
-                                    <button className="bg-red-500 text-white px-3 py-1 rounded-full">
-                                        Chỉnh sửa
-                                    </button>
+                                    <h3 className="text-xl font-semibold">Tên đăng nhập</h3>
+                                    <input
+                                        type="text"
+                                        placeholder='Tên đăng nhập'
+                                        className="border-b border-gray-200 focus:outline-none py-[5px] px-[10px] w-[200%]"
+                                    />
                                 </div>
                             </div>
                         </div>
                     </div>
                 </main>
+            </div>
+            <div>
+                <Footer />
             </div>
         </div>
     );
