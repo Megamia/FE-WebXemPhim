402550f3a25432e4b1bf9abd1538e37f313a0f88
Megamia <luudat214@gmaill.com>
2024-03-18 13:57:52 +0700

    update donate

--- src/Component/Donate/Donate.jsx
+++ src/Component/Donate/Donate.jsx
@@ -1,50 +1,67 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Header from "../Header&Footer/Header/Header";
 import Footer from "../Header&Footer/Footer/Footer";
 import Paypal from "./Paypal";
+import axios from "axios";
 
 const Donate = () => {
   const [selectedPrice, setSelectedPrice] = useState(null);
   const [showPaypal, setShowPaypal] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [description, setDescription] = useState("");
+  const [donateData, setDonateData] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/donate`)
+      .then(function (response) {
+        console.log(response.data);
+        setDonateData(response.data.donates);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
   const handlePriceChange = (price) => {
-    const chosenProduct = products.find((product) => product.price === price);
+    const chosenProduct = donateData.find((product) => product.price === price);
     setSelectedPrice(price);
     setDescription(chosenProduct.description); // Set description here
     setShowModal(true);
+    setShowPaypal(false);
   };
+
+
+  // const handleDescriptionChange = (event) => {
+  //   setDescription(event.target.value);
+  // };
   
-
-  const handleDescriptionChange = (event) => {
-    setDescription(event.target.value);
-  };
-
+  const handleRefuse = () => {
+    setShowModal(false); 
+    setShowPaypal(false);
+  }
   const handleDescriptionSubmit = () => {
     // Validate description (optional)
-
     setShowModal(false); // Close description modal
     setShowPaypal(true); // Open Paypal modal with description
   };
 
-  const products = [
-    {
-      price: "1.99",
-      image: "../../img/donate1.jpg",
-      description: "Đáy xã hội",
-    },
-    {
-      price: "3.99",
-      image: "../../img/donate2.jpg",
-      description: "Thường dân",
-    },
-    {
-      price: "5.99",
-      image: "../../img/donate3.jpg",
-      description: "Quý tộc",
-    },
-  ];
+  // const products = [
+  //   {
+  //     price: "1.99",
+  //     image: "../../img/donate1.jpg",
+  //     description: "Đáy xã hội",
+  //   },
+  //   {
+  //     price: "3.99",
+  //     image: "../../img/donate2.jpg",
+  //     description: "Thường dân",
+  //   },
+  //   {
+  //     price: "5.99",
+  //     image: "../../img/donate3.jpg",
+  //     description: "Quý tộc",
+  //   },
+  // ];
 
   return (
     <>
@@ -57,17 +74,16 @@ const Donate = () => {
             </div>
             <div className="flex flex-col items-center ">
               <div className="flex flex-row mt-[30px]">
-                {products.map((product) => (
+                {donateData.map((product) => (
                   <div className="px-[30px]">
                     <div
-                      className={`text-white cursor-pointer ${
-                        selectedPrice === product.price ? "border-red-500" : ""
-                      }`}
+                      className={`text-white cursor-pointer ${selectedPrice === product.price ? "border-red-500" : ""
+                        }`}
                       key={product.price}
                       onClick={() => handlePriceChange(product.price)}
                     >
                       <img
-                        src={product.image}
+                        src={`../../img/${product.img}`}
                         alt={`$${product.price}`}
                         className="w-[200px] h-[300px] "
                       />
@@ -95,7 +111,7 @@ const Donate = () => {
                     <div className="flex mt-4 justify-between items-center">
                       <button
                         className="bg-gray-300 w-[150px] text-black p-2 rounded-md"
-                        onClick={() => setShowModal(false)}
+                        onClick={(handleRefuse)}
                       >
                         Đéo
                       </button>
